743e60394696a2c94bc3efb3b67db20dcfaec2f1
antonlovg <karlanton.lovgren@gmail.com>
2023-09-27 18:35:36 +0200

    Kommenterat och formaterat modulen

--- Inlämningsuppgift/ui.py
+++ Inlämningsuppgift/ui.py
@@ -4,19 +4,23 @@ import os
 ui_width = 30
 
 
+# Skapar linjer
 def linjer():
     print("-" * ui_width)
 
 
+# Skapar title
 def title(text1, text2):
     print(".:".ljust(5), text1.center(20), ":.".rjust(4))
     print(".:".ljust(5), text2.center(20), ":.".rjust(4))
 
 
+# Skapar valmöjligheter
 def val(text):
     return input("| " + text + " > ")
 
 
+# Rensar terminalen
 def clear():
     if os.name == "nt":
         os.system("cls")
@@ -25,9 +29,11 @@ def clear():
         os.system("clear")
 
 
+# Skapar utseende för val
 def menyval(text1, text2):
     print("|", text1, f"|\t{text2}")
 
 
+# Alla övrigt som behöver skrivas ut
 def övrigt(text):
     print("| " + text)
